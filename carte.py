53c30c4e36e14a43a548ef8ae696a4f2d31dd45b
Pascal <pascal.schwartz@protonmail.com>
2022-12-05 12:01:28 +0100

    add delete functionality

--- carte.py
+++ carte.py
@@ -1,18 +1,26 @@
 run = True
 carte = {"Cola": 100}
+carte_list = []
 
-with open('cart.txt') as f:
-    for line in f:
-        current = line.replace("\n", "")
-        item = current.split(":", 1)
-        carte.update({item[0]: item[1]})
+try:
+    with open('cart.txt') as f:
+        i = 0
+        for line in f:
+            item = line.replace("\n", "").split(",", 1)
+            carte.update({item[0]: item[1]})
+            carte_list.append(item[0])
+except:
+    with open('cart.txt', 'w') as f:
+        f.write("")
 
 
 # Print the carte
 def print_carte():
     print("Speisekarte:")
+    i = 1
     for x in carte:
-        print(x + " - " + str(carte.get(x)))
+        print(str(i) + ". " + x + " - " + str(carte.get(x)))
+        i += 1
 
 
 # Add products to the carte
@@ -32,9 +40,29 @@ def add_to_carte():
         price = int(price)
 
     except ValueError:
-        print("Nicht erlaubte Eingabe")
+        print("Keine korrekte Eingabe")
+        return
 
     carte.update({name: price})
+    carte_list.append(name)
+
+#Delete dish from carte
+def delete_from_carte():
+    print_carte()
+    print("Bitte geben Sie die Nr. des Gerichts ein das sie entfernen moechten.")
+
+    try:
+        delete = int(input())
+    except ValueError:
+        print("Keine Korrekte Eingabe, kehre zurück ins Hauptmenue.")
+        return
+
+    if delete > len(carte_list) or delete < 0:
+        print("Gericht nicht vorhanden.")
+        return
+    else:
+        carte.pop(carte_list[delete - 1])
+        carte_list.remove(carte_list[delete - 1])
 
 
 # Loop through the menu until user decides to quit
@@ -43,15 +71,19 @@ while run:
           "\n"
           "a = Speisekarte anzeigen\n"
           "n = neues Gericht hinzufügen\n"
-          "e = Programmende")
-    userInput = input()
+          "l = Gericht löschen\n"
+          "e = Programmende\n")
 
-    if userInput == "a":
+    user_input = input()
+
+    if user_input == "a":
         print_carte()
-    if userInput == "n":
+    if user_input == "n":
         add_to_carte()
-    if userInput == "e":
+    if user_input == "l":
+        delete_from_carte()
+    if user_input == "e":
         with open('cart.txt', 'w') as f:
             for name, price in carte.items():
-                f.write('%s:%s\n' % (name, price))
+                f.write('%s,%s\n' % (name, price))
         run = False
